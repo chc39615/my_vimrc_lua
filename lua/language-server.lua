788b7e0a3cd2f40153545782d70f97aaf6313192
Yaoyuan <chc39615@gmail.com>
2022-11-20 17:47:55 +0800

    language-server modified

--- lua/language-server.lua
+++ lua/language-server.lua
@@ -88,14 +88,69 @@ end
 ------------------ config lsp-config ------------------
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
+-- This function gets run when an LSP connects to a particular buffer
+local on_attach = function(_, bufnr)
+    -- NOTE: Remember that lua is a real programming language, and as such it is possible
+    -- to define small helper and utility functions so you don't have to repeat yourself
+    -- many times.
+    -- In this case, we create a function that lets us more easily define mappinds specific
+    -- for LSP related items. It sets the mode, buffer and description for us each time.
+    local nmap = function (keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[Code] [A]ction')
+
+    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+    nmap('gr', require("telescope.builtin").lsp_references)
+    nmap('<leader>ds', require("telescope.builtin").lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>ws', require("telescope.builtin").lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+    -- See `:help K` for why this keymap
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('<C-k', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+    -- Lesser used LSP functionality
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder]')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end,'[W]orkspace [L]ist Folders')
+
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        if vim.lsp.buf.format then
+            vim.lsp.buf.format()
+        elseif vim.lsp.buf.formatting then
+            vim.lsp.buf.formatting()
+        end
+    end, { desc = 'Format current buffer with LSP' })
+
+end
+
 local lspconfig = require("lspconfig")
 
+-- make runtime files discoverable to the server
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
 lspconfig.sumneko_lua.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
     settings = {
         Lua = {
             runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = 'LuaJIT',
+                -- setup your lua path
+                path = runtime_path,
             },
             diagnostics = {
                 globals = { 'vim', }
@@ -108,19 +163,21 @@ lspconfig.sumneko_lua.setup {
             telemetry = {
                 enable = false,
             },
-            capabilities = capabilities,
         },
     },
 }
 
 lspconfig.pyright.setup {
-    python = {
-        analysis = {
-            autoSearchPaths = true,
-            diagnosticMode = "workspace",
-            useLibraryCodeForTypes = true
-        },
-        capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "workspace",
+                useLibraryCodeForTypes = true,
+            },
+        }
     }
 }
 
