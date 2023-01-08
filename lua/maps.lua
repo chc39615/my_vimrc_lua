b2e515fccf834eed87d05a963cfe6e3793af3cb7
cody_zhang <cody_zhang@askey.com>
2023-01-08 15:17:28 +0800

    fix treesitter problem

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,6 +1,5 @@
 local map = vim.api.nvim_set_keymap
 
-
 -- map the leader key
 map('n', '<Space>', '', {})
 vim.g.mapleader = ' ' -- 'vim.g sets global variables
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -87,10 +87,15 @@ return require('packer').startup(function(use)
             highlight = {
                 -- `false` will disable the whole extension
                 enable = true,
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time
+                -- Set this true if youi depend on 'syntax' being enabled (like for indentation)
+                -- Using this option may slow down your editor, and yoiu may see some duplicate highlights
+                -- Instead of true it can also be a list of languades
+                additional_vim_regex_highlighting = false,
             },
 
             indent = {
-                enable = true,
+                enable = true, disable = { "python" },
             },
 
         }
