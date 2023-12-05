b108f6eb2f12ab490cd47e8896ba2afc2145295f
Yaoyuan <chc39615@gmail.com>
2023-12-05 11:17:12 +0800

    temp

--- init.lua
+++ init.lua
@@ -38,3 +38,4 @@ require("lazy").setup({
 })
 
 require("config").setup()
+require("config").init()
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,15 @@
 return {
+	{
 	"marko-cerovac/material.nvim",
 	lazy = true,
 	opts = { style = "oceanic" },
+},
+{
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {} 
+
 }
+}
+
