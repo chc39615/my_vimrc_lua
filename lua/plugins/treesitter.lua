01a63d11a4a92c020bcceebd341f68bd4ff052a0
yaoyuan <chc39615@gmail.com>
2023-12-08 10:03:05 +0800

    modify treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,61 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			require("nvim-treesitter.install").update({ with_sync = true })
+		build = ":TSUpdate",
+		-- event = { "LazyFile", "VeryLazy" },
+		event = { "BufReadPost", "BufNewfile", "VeryLazy" },
+		init = function(plugin)
+			-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+			-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+			-- no longer trigger the **nvim-treeitter** module to be loaded in time.
+			-- Luckily, the only thins that those plugins need are the custom queries, which we make available
+			-- during startup.
+			require("lazy.core.loader").add_to_rtp(plugin)
+			require("nvim-treesitter.query_predicates")
 		end,
-		event = { "BufReadPost", "BufNewfile" },
 		dependencies = {
 			{
 				"nvim-treesitter/nvim-treesitter-textobjects",
-				init = function()
-					-- PERF: no need to load the plugin, if we only need its queries for mini.ai
-					local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
-					local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-					local enabled = false
-					if opts.textobjects then
-						for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-							if opts.textobjects[mod] and opts.textobjects[mod].enable then
-								enabled = true
-								break
+				config = function()
+					-- When in diff mode, we want to use the default
+					-- vim text objects c & C instead of the treesitter ones.
+					local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+					local configs = require("nvim-treesitter.configs")
+					for name, fn in pairs(move) do
+						if name:find("goto") == 1 then
+							move[name] = function(q, ...)
+								if vim.wo.diff then
+									local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+									for key, query in pairs(config or {}) do
+										if q == query and key:find("[%]%[][cC]") then
+											vim.cmd("normal! " .. key)
+											return
+										end
+									end
+								end
+								return fn(q, ...)
 							end
 						end
 					end
-
-					if not enabled then
-						require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-					end
 				end,
+				-- init = function()
+				-- 	-- PERF: no need to load the plugin, if we only need its queries for mini.ai
+				-- 	local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+				-- 	local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+				-- 	local enabled = false
+				-- 	if opts.textobjects then
+				-- 		for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+				-- 			if opts.textobjects[mod] and opts.textobjects[mod].enable then
+				-- 				enabled = true
+				-- 				break
+				-- 			end
+				-- 		end
+				-- 	end
+				--
+				-- 	if not enabled then
+				-- 		require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+				-- 	end
+				-- end,
 			},
 		},
 		keys = {
@@ -34,6 +64,8 @@ return {
 		},
 		---@type TSConfig
 		opts = {
+			modules = {},
+			ignore_install = {},
 			highlight = {
 				-- `false` will disable the whole extension
 				enable = true,
@@ -45,7 +77,7 @@ return {
 			},
 			indent = {
 				enable = true,
-				disable = { "python" },
+				-- disable = { "python" },
 			},
 			context_commentstring = { enable = true, enable_autocmd = false },
 			ensure_installed = {
